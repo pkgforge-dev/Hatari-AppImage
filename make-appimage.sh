e33a07b453d049b6c76d1ca6992a2857afea43ca
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:01:27 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,10 +3,10 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q hatari | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q hatari | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/hatari.svg
 export DESKTOP=/usr/share/applications/hatari.desktop
@@ -16,11 +16,9 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun /usr/bin/hatari
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
