fbe9f296a0ed9a340f35dc84aaab6b659245790c
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:38:21 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -16,8 +16,6 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun /usr/bin/hatari
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
